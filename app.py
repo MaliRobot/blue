9bc1600f581064146cd3f2d04eafe3e2c518316d
MaliRobot <milos.trifunovic@gmail.com>
2019-05-25 19:55:43 +0200

    auth required

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 from decouple import config
 from flask_mysqldb import MySQL
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 from os import getenv
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = getenv('SECRET_KEY', config('SECRET_KEY'))
+api = Api(app)
+
 app.config['MYSQL_HOST'] = getenv('DB_HOST', config('DB_HOST'))
 app.config['MYSQL_USER'] = getenv('DB_USER', config('DB_USER'))
 app.config['MYSQL_PASSWORD'] = getenv('DB_PASSWORD', config('DB_PASSWORD'))
@@ -14,12 +19,14 @@ app.config['MYSQL_DB'] = getenv('DB_NAME', config('DB_NAME'))
 mysql = MySQL()
 mysql.init_app(app)
 
-api = Api(app)
+app.config['SECRET_KEY'] = getenv('SECRET_KEY', config('SECRET_KEY'))
+jwt = JWTManager(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('name', type=str, help='Rate to charge for this resource')
-parser.add_argument('password', type=str, help='Rate to charge for this resource')
-parser.add_argument('admin', type=bool, help='Rate to charge for this resource')
+parser.add_argument('username', type=str)
+parser.add_argument('password', type=str)
+parser.add_argument('admin', type=bool)
+parser.add_argument('email', type=str)
 
 
 class User(Resource):
@@ -28,35 +35,35 @@ class User(Resource):
         sql = '''SELECT * FROM users WHERE id = %s'''
         cur.execute(sql, (user_id,))
         row = cur.fetchone()
+        cur.close()
 
         if row is not None:
-            result = {'user': row[1], 'admin': row[3]}
+            return jsonify({'username': row[1], 'admin': row[3], 'email': row[4]}), 200
         else:
-            result = {'Error': 'No user with that id'}
-        cur.close()
-        return result
+            return jsonify({'Error': 'No user with that id'}), 404
 
+    @jwt_required
     def put(self, user_id):
         args = parser.parse_args()
-        if 'name' in args and 'password' in args:
+        if 'password' in args and 'email' in args:
             cur = mysql.connection.cursor()
             if 'admin' in args:
                 admin = args['admin']
-                sql = '''UPDATE users set username = %s, password = %s, admin = %s WHERE id %s'''
-                cur.execute(sql, (args['name'], args['password'], admin, user_id))
+                sql = '''UPDATE users set password = %s, admin = %s, email = %s WHERE id %s'''
+                cur.execute(sql, (args['password'], admin, args['email'], user_id))
             else:
-                sql = '''UPDATE users set username = %s, password = %s WHERE id %s'''
-                cur.execute(sql, (args['name'], args['password'], user_id))
+                sql = '''UPDATE users set password = %s WHERE id %s'''
+                cur.execute(sql, (args['password'], args['email'], user_id))
             mysql.connection.commit()
             cur.close()
             if cur.rowcount:
-                result = {"Success": "Updated user with id {}.".format(user_id)}
+                return jsonify({"Success": "Updated user with id {}.".format(user_id)}), 200
             else:
-                result = {"Error": "Couldn't find or couldn't delete user with id {}.".format(user_id)}
+                return({"Error": "Couldn't find or couldn't delete user with id {}.".format(user_id)}), 400
         else:
-            result = {'Error': "Not enough parameters to create a user"}
-        return result
+            return jsonify({'Error': "Not enough parameters to create a user"}), 400
 
+    @jwt_required
     def delete(self, user_id):
         cur = mysql.connection.cursor()
         sql = '''DELETE FROM users WHERE id = %s'''
@@ -64,13 +71,16 @@ class User(Resource):
         mysql.connection.commit()
         cur.close()
         if cur.rowcount:
-            return {"Success": "Deleted user with id {}.".format(user_id)}
+            return jsonify({"msg": "Deleted user with id {}.".format(user_id)}), 204
         else:
-            return {"Error": "Couldn't find or couldn't delete user with id {}.".format(user_id)}
+            return jsonify({"err": "Couldn't find or couldn't delete user with id {}.".format(user_id)}), 404
 
 
 class Users(Resource):
+    @jwt_required
     def get(self):
+        current_user = get_jwt_identity()
+        print(current_user)
         cur = mysql.connection.cursor()
         cur.execute('''SELECT * FROM users''')
         rows = cur.fetchall()
@@ -78,26 +88,55 @@ class Users(Resource):
         if rows is not None:
             result = []
             for row in rows:
-                result.append({'name': row[1], 'id': row[0], 'admin': row[3]})
+                result.append({'username': row[1], 'id': row[0], 'admin': row[3], 'email': row[4]})
+            return jsonify(result)
         else:
-            result = {'Error': 'There are no users'}
-        return result
+            return jsonify({'err': 'There are no users'}), 404
 
+    @jwt_required
     def post(self):
         args = parser.parse_args()
-        if 'name' in args and 'password' in args:
+        if 'name' in args and 'password' in args and 'email' in args:
             admin = False
             if 'admin' in args:
                 admin = args['admin']
             cur = mysql.connection.cursor()
-            sql = '''INSERT INTO users (username, password, admin) VALUES (%s, %s, %s)'''
-            cur.execute(sql, (args['name'], args['password'], admin,))
+            sql = '''INSERT INTO users (username, password, admin, email) VALUES (%s, %s, %s, %s)'''
+            cur.execute(sql, (args['username'], args['password'], admin, args['email'],))
             mysql.connection.commit()
             cur.close()
-            result = {'Success': 'User inserted'}
+            result = jsonify({'msg': 'User inserted'}), 201
         else:
-            result = {'Error': "Not enough parameters to create a user"}
+            result = jsonify({'err': "Not enough parameters to create a user"}), 400
         return result
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    current_user = get_jwt_identity()
+    print(current_user)
+
+    args = parser.parse_args()
+
+    if 'username' not in args or 'password' not in args:
+        return jsonify({"err": "You must provide user and password parameters"}), 400
+    username = args['username']
+    password = args['password']
+
+    cur = mysql.connection.cursor()
+    sql = '''SELECT password FROM users WHERE username = %s'''
+    cur.execute(sql, (username,))
+    row = cur.fetchone()
+    mysql.connection.commit()
+    cur.close()
+
+    if row:
+        if row[0] == password:
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token), 200
+        return jsonify({"err": "Wrong password."}), 401
+    else:
+        return jsonify({"err": "No user with that username"}), 404
 
 
 @app.route('/')
